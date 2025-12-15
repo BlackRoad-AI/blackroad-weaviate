bb150a04e2f10c44b4dccc44c7204cd00db7b83b
moogacs <contact@m-mooga.com>
2025-12-15 14:28:54 +0100

    refact(replication): introduce replication error pkg and unify erroring

--- usecases/replica/replicator.go
+++ usecases/replica/replicator.go
@@ -25,6 +25,7 @@ import (
 	"github.com/weaviate/weaviate/entities/storobj"
 	"github.com/weaviate/weaviate/usecases/monitoring"
 	"github.com/weaviate/weaviate/usecases/objects"
+	replicaerrors "github.com/weaviate/weaviate/usecases/replica/errors"
 )
 
 // opID operation encode as and int
@@ -125,13 +126,13 @@ func (r *Replicator) PutObject(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.one").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		return fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
-
+		return replicaerrors.NewReplicasErrorWithLevel(l, err)
 	}
 	err = r.stream.readErrors(1, level, replyCh)[0]
 	if err != nil {
 		r.log.WithField("op", "put").WithField("class", r.class).
 			WithField("shard", shard).WithField("uuid", obj.ID()).Error(err)
+		err = replicaerrors.NewReplicasErrorWithLevel(l, err)
 	}
 	return err
 }
@@ -157,14 +158,14 @@ func (r *Replicator) MergeObject(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.merge").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		return fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
+		return replicaerrors.NewReplicasErrorWithLevel(l, err)
 	}
 	err = r.stream.readErrors(1, level, replyCh)[0]
 	if err != nil {
 		r.log.WithField("op", "merge").WithField("class", r.class).
 			WithField("shard", shard).WithField("uuid", doc.ID).Error(err)
-		var replicaErr *Error
-		if errors.As(err, &replicaErr) && replicaErr != nil && replicaErr.Code == StatusObjectNotFound {
+		var replicaErr *replicaerrors.Error
+		if errors.As(err, &replicaErr) && replicaErr != nil && replicaErr.Code == replicaerrors.StatusObjectNotFound {
 			return objects.NewErrDirtyWriteOfDeletedObject(replicaErr)
 		}
 	}
@@ -193,7 +194,7 @@ func (r *Replicator) DeleteObject(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.delete").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		return fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
+		return replicaerrors.NewReplicasErrorWithLevel(l, err)
 	}
 	err = r.stream.readErrors(1, level, replyCh)[0]
 	if err != nil {
@@ -225,7 +226,7 @@ func (r *Replicator) PutObjects(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.many").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		err = fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
+		err = replicaerrors.NewReplicasErrorWithLevel(l, err)
 		errs := make([]error, len(objs))
 		for i := 0; i < len(objs); i++ {
 			errs[i] = err
@@ -275,7 +276,7 @@ func (r *Replicator) DeleteObjects(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.deletes").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		err = fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
+		err = replicaerrors.NewReplicasErrorWithLevel(l, err)
 		errs := make([]objects.BatchSimpleObject, len(uuids))
 		for i := 0; i < len(uuids); i++ {
 			errs[i].Err = err
@@ -311,7 +312,7 @@ func (r *Replicator) AddReferences(ctx context.Context,
 	if err != nil {
 		r.log.WithField("op", "push.refs").WithField("class", r.class).
 			WithField("shard", shard).Error(err)
-		err = fmt.Errorf("%s %q: %w", MsgCLevel, l, ErrReplicas)
+		err = replicaerrors.NewReplicasErrorWithLevel(l, err)
 		errs := make([]error, len(refs))
 		for i := 0; i < len(refs); i++ {
 			errs[i] = err
